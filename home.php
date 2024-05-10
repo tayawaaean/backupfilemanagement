cecf567d9cfda8db27ca76d875b6279deec8d67f
tayawaaean <143526706+tayawaaean@users.noreply.github.com>
2024-05-11 00:15:47 +0800

    s

--- home.php
+++ home.php
@@ -4,35 +4,65 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
+
 <div class="dash-content">
     <div class="overview">
         <div class="title">
             <h2>Dashboard</h2>
         </div>
         <hr>
-        <div class="boxes">
-            <div class="box box1">
-                <i class='bx bxs-user'></i>
-                <span class="text">
-                    <span class="number">12</span>
-                    <span class="text">Total Users</span>
-                </span>
-            </div>
-        	<div class="box box2">
-                <i2 class="material-symbols-outlined">folder_open</i2>
-                <span class="text">
-                    <span class="number">1,028</span>
-                    <span class="text">Total Files</span>
-                </span>
-        	</div>
-            <div class="box box3">
-                <i class="material-symbols-outlined">app_registration</i>
-                <span class="text">
-                    <span class="number">4</span>
-                    <span class="text">Pending Register</span>
-                </span>
-            </div>
+        <?php if($_SESSION['login_type'] == 1): ?>
+    <div class="boxes">
+        <div class="box box1">
+            <i class='bx bxs-user'></i>
+            <span class="text">
+                <span class="number">12</span>
+                <span class="text">Total Users</span>
+            </span>
         </div>
+        <div class="box box2">
+            <i2 class="material-symbols-outlined">folder_open</i2>
+            <span class="text">
+                <span class="number">1,028</span>
+                <span class="text">Total Files</span>
+            </span>
+        </div>
+        <div class="box box3">
+            <i class="material-symbols-outlined">app_registration</i>
+            <span class="text">
+                <span class="number">4</span>
+                <span class="text">Pending Register</span>
+            </span>
+        </div>
+    </div>
+<?php endif; ?>
+<?php if($_SESSION['login_type'] == 2): ?>
+    <div class="boxes">
+        <div class="box box1">
+            <i class='bx bxs-user'></i>
+            <span class="text">
+                <span class="number">12</span>
+                <span class="text">Total Files</span>
+            </span>
+        </div>
+        
+        <div class="box box3">
+            <i class="material-symbols-outlined">app_registration</i>
+            <span class="text">
+                <span class="number">4</span>
+                <span class="text">Folders</span>
+            </span>
+        </div>
+		<div class="box box2">
+            <i2 class="material-symbols-outlined">folder_open</i2>
+            <span class="text">
+                <span class="number">1,028</span>
+                <span class="text">Shared Files</span>
+            </span>
+        </div>
+    </div>
+<?php endif; ?>
+
     </div>
     <div class="container">
         <div class="activity">
@@ -71,7 +101,7 @@
 				<div class="bottom-search">
 					<div class="search">
 						<i class="uil uil-search"></i>
-						<input type="text" placeholder="Search...">
+						<input id="searchInput" type="text" placeholder="Search...">
 					</div>
 				</div>
             </div>
@@ -235,5 +265,24 @@
 		</div>
 	</div>
 </div>
+<script>
+document.getElementById('dropdown2').addEventListener('change', function() {
+    var selectedMonth = this.value;
+    filterTableByDate(selectedMonth);
+});
 
-<script src= "./js/home.js"></script>
+function filterTableByDate(selectedMonth) {
+    var tableRows = document.querySelectorAll('.table tbody tr');
+    tableRows.forEach(function(row) {
+        var dateCell = row.querySelector('td:nth-child(2)').textContent;
+        var month = dateCell.split(' ')[0]; // Extract the month from the date cell
+        if (month !== selectedMonth) {
+            row.style.display = 'none'; // Hide rows that do not match the selected month
+        } else {
+            row.style.display = ''; // Show rows that match the selected month
+        }
+    });
+}
+
+</script>
+
--- navbar.php
+++ navbar.php
@@ -5,8 +5,12 @@
 <nav id="sidebar" class='mx-lt-5 bg-dark' >
 		
 		<div class="sidebar-list">
+<<<<<<< Updated upstream
 				<?php if($_SESSION['login_type'] == 1): ?>
 				<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Home</a>
+=======
+			<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Home</a>
+>>>>>>> Stashed changes
 				<a href="index.php?page=files" class="nav-item nav-files"><span class='icon-field'><i class="fa fa-file"></i></span> Files</a>
 				<a href="index.php?page=users" class="nav-item nav-users"><span class='icon-field'><i class="fa fa-users"></i></span> Users</a>
 			<?php endif; ?>
@@ -15,6 +19,7 @@
 				<a href="index.php?page=files" class="nav-item nav-files"><span class='icon-field'><i class="fa fa-file"></i></span> Files</a>
 				<a href="index.php?page=personal" class="nav-item nav-users"><span class='icon-field'><i class="fa fa-users"></i></span> Personal Information</a>
 			<?php endif; ?>
+			<a href="index.php?page=shared-files" class="nav-item nav-shared"><span class='icon-field'><i class="fa fa-file"></i></span>Shared Files</a>
 		</div>
 
 </nav>
