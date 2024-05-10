6f6f90ef3852e7dd71e230f9349ca8efbc9c96ff
SelwynePonce <141317372+SelwynePonce@users.noreply.github.com>
2024-05-10 17:31:53 +0800

    Login/Signup

---
+++ js/login.js
@@ -0,0 +1,31 @@
+$('#login-form').submit(function(e){
+    e.preventDefault()
+
+    $('#Login').attr('disabled',true).html('Logging in...');
+    if($(this).find('.alert-danger').length > 0 )
+        $(this).find('.alert-danger').remove();
+    $.ajax({
+        url:'ajax.php?action=login',
+        method:'POST',
+        data:$(this).serialize(),
+        error:err=>{
+            console.log(err)
+    $('#Login').removeAttr('disabled').html('Login');
+
+        },
+        success:function(resp){
+            if(resp == 1){
+                location.reload('index.php?page=home');
+            }else{
+                $('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+                $('#Login').removeAttr('disabled').html('Login');
+            }
+        }
+    })
+})
+
+// JavaScript code to handle the signup button click event
+document.getElementById("Signup").addEventListener("click", function() {
+    document.getElementById("login-section").style.display = "none";
+    document.getElementById("signup-section").style.display = "block";
+});
--- login.php
+++ login.php
@@ -5,62 +5,23 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Admin | Blog Site</title>
+  <title>Bingao National High School</title>
+  <link rel="stylesheet" href= "./css/login.css">
+  <link rel="icon" href="./assets/img/BNHS Logo.png">
  	
 
-<?php include('./header.php'); ?>
-<?php 
-session_start();
-if(isset($_SESSION['login_id']))
-header("location:index.php?page=home");
-?>
+	<?php include('./header.php'); ?>
+
+	<?php 
+		session_start();
+	
+		if(isset($_SESSION['login_id']))
+		header("location:index.php?page=home");
+	?>
 
 </head>
-<style>
-	body{
-		width: 100%;
-	    height: calc(100%);
-	    /*background: #007bff;*/
-	}
-	main#main{
-		width:100%;
-		height: calc(100%);
-		background:white;
-	}
-	#login-right{
-		position: absolute;
-		right:0;
-		width:40%;
-		height: calc(100%);
-		background:white;
-		display: flex;
-		align-items: center;
-	}
-	#login-left{
-		position: absolute;
-		left:0;
-		width:60%;
-		height: calc(100%);
-		background:#00000061;
-		display: flex;
-		align-items: center;
-	}
-	#login-right .card{
-		margin: auto
-	}
-	.logo {
-    margin: auto;
-    font-size: 8rem;
-    background: white;
-    padding: .5em 0.8em;
-    border-radius: 50% 50%;
-    color: #000000b3;
-}
-</style>
 
 <body>
-
-
   <main id="main" class=" alert-info">
   		<div id="login-left">
   			<div class="logo">
@@ -68,54 +29,85 @@ header("location:index.php?page=home");
   			</div>
   		</div>
   		<div id="login-right">
-  			<div class="card col-md-8">
-  				<div class="card-body">
+  			<div class="card col-md-8 container-card">
+				<!-- Login -->
+  				<div class="card-body" id= "login-section">
+					<div class= "logo-container">
+						<img src= "./assets/img/BNHS Logo.png" width= 150 height=150>
+						<div class="text">
+							<p>Hello,</p>
+							<p id='bold'>Welcome!</p>
+							<small>Log in to your account to continue</small>
+						</div>
+					</div>
+
   					<form id="login-form" >
+
   						<div class="form-group">
-  							<label for="username" class="control-label">Username</label>
-  							<input type="text" id="username" name="username" class="form-control">
+  							<input type="text" id="username" name="username" required>
+							<span>Username</span>
   						</div>
   						<div class="form-group">
-  							<label for="password" class="control-label">Password</label>
-  							<input type="password" id="password" name="password" class="form-control">
+  							<input type="password" id="password" name="password" required>
+							  <span>Password</span>
   						</div>
-  						<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+						<div class="center-button">
+							<button class="btn-sm col-md-6" id= "Login">Login</button>
+							<button class="btn-sm col-md-6" id= "Signup" type="button">Signup</button>
+						</div>
   					</form>
+
+					<div class="forgot">
+						<button class= "btn-sm forgotpass" id= "forgotpass" >Forgot Password</button>
+					</div>
+  				</div>
+
+				<!-- Signup -->
+				<div class="card-body signup" id= "signup-section">
+					<div class= "logo-container">
+						<img src= "./assets/img/BNHS Logo.png" width= 150 height=150>
+						<div class="text">
+							<p id='bold'>Sign Up</p>
+							<small>Create your Account to continue</small>
+						</div>
+					</div>
+
+  					<form id="signup-form" >
+					  	<div class="form-group">
+  							<input type="text" id="name" name="name" required>
+							<span>Name</span>
+  						</div>
+
+						<div class="form-group">
+  							<input type="text" id="email" name="email" required>
+							<span>Email</span>
+  						</div>
+
+  						<div class="form-group">
+  							<input type="text" id="username2" name="username2" required>
+							<span>Username</span>
+  						</div>
+  						<div class="form-group">
+  							<input type="password" id="password2" name="password2" required>
+							  <span>Password</span>
+  						</div>
+						<div class="center-button">
+							<button class="btn-sm col-md-12" id= "Sign-in" type="submit">Sign Up</button>
+						</div>
+  					</form>
+
+					<div class="forgot">
+						<text>Already have an account?&nbsp</text>
+						<a class= "forgotpass" href="login.php">Login<a/>
+					</div>
   				</div>
   			</div>
   		</div>
-   
-
   </main>
 
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
-
 </body>
-<script>
-	$('#login-form').submit(function(e){
-		e.preventDefault()
-		$('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
-		if($(this).find('.alert-danger').length > 0 )
-			$(this).find('.alert-danger').remove();
-		$.ajax({
-			url:'ajax.php?action=login',
-			method:'POST',
-			data:$(this).serialize(),
-			error:err=>{
-				console.log(err)
-		$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
 
-			},
-			success:function(resp){
-				if(resp == 1){
-					location.reload('index.php?page=home');
-				}else{
-					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
-					$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
-				}
-			}
-		})
-	})
-</script>	
+<script src= "./js/login.js"></script>	
 </html>
