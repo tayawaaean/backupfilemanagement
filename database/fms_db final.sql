69c605a939d268d18d9be0d6a940174bbdd6d15b
tayawaaean <143526706+tayawaaean@users.noreply.github.com>
2024-05-29 15:33:54 +0800

    final db

--- database/fms_db final.sql
+++ database/fms_db final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2024 at 06:25 PM
+-- Generation Time: May 29, 2024 at 09:33 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,26 +41,13 @@ CREATE TABLE `activity_log` (
 --
 
 INSERT INTO `activity_log` (`id`, `Author`, `job_title`, `DateTime`, `Action`, `Description`) VALUES
-(1, 'Aean Gabrielle Tayawa', 'Admin', '2024-04-17 00:38:17', 'New User Approved', 'Dexter John Perdido'),
-(2, 'Dexter John Perdido', 'User', '2024-04-17 18:38:17', 'Document Deleted', 'MMSU Waiver.docx'),
-(3, 'Kenric Catiwa', 'User', '2024-04-17 18:43:14', 'Document Upload', 'Ma\'am Abbie Assignment.pdf'),
-(18, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-04-20 23:14:07', 'New User Approved', 'Kimberly Mae B. Reodique'),
-(21, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-04-20 23:31:29', 'New User Approved', 'Ryan Anthony Gabriel B. Adaya'),
-(22, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-04-20 23:34:38', 'New User Denied', 'Ryan Anthony Gabriel B. Adaya'),
-(23, 'mekel', 'Employee', '2024-04-22 14:59:00', 'Profile Updated', 'mekel'),
-(24, 'Micheal Jay A. Pedronan', 'Employee', '2024-04-22 15:51:17', 'Profile Updated', 'Micheal Jay A. Pedronan'),
-(25, 'Micheal Jay A. Pedronan', 'Employee', '2024-04-22 22:10:16', 'Profile Updated', 'Micheal Jay A. Pedronan'),
-(26, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-04-22 22:13:36', 'New User Approved', 'Kimberly Mae B. Reodique'),
-(27, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-04-22 22:15:00', 'New User Approved', 'Kimberly Mae B. Reodique'),
-(28, 'Micheal Jay A. Pedronan', 'Employee', '2024-05-01 21:52:16', 'Profile Updated', 'Micheal Jay A. Pedronan'),
-(29, 'Micheal Jay A. Pedronan', '', '2024-05-01 23:52:09', 'Uploaded a new file', 'test'),
-(30, 'Micheal Jay A. Pedronan', 'Employee', '2024-05-01 23:54:23', 'Uploaded a new file', 'test'),
-(31, 'Micheal Jay A. Pedronan', 'Employee', '2024-05-02 01:48:20', 'Uploaded Multiple Files', 'test'),
-(32, 'Micheal Jay A. Pedronan', 'Employee', '2024-05-02 02:04:06', 'Created A New Folder', 'New Folder'),
-(33, 'Micheal Jay A. Pedronan', 'Employee', '2024-05-07 20:50:37', 'Uploaded a new file', ''),
-(34, 'Micheal Jay A. Pedronan', 'Employee', '2024-05-07 20:58:04', 'Uploaded a new file', 'test'),
-(35, 'Micheal Jay A. Pedronan', 'Employee', '2024-05-07 21:05:57', 'Created A New Folder', 'final test'),
-(36, 'Micheal Jay A. Pedronan', 'Employee', '2024-05-10 02:06:19', 'Created A New Folder', 'as');
+(43, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-05-24 17:35:41', 'Document Deleted', 'Deleted FORM 8 WITH ENGINEER and COE in Test folder'),
+(44, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-05-24 17:38:06', 'Folder Deleted', 'Deleted folder Test'),
+(45, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-05-24 17:41:57', 'File Uploaded', 'Uploaded file FORM 8 WITH ENGINEER and COE.docx in folder ID 14'),
+(46, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-05-24 17:52:34', 'Folder Created', 'Created folder Okay'),
+(47, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-05-24 17:52:54', 'Folder Updated', 'Updated folder Okayed'),
+(48, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-05-24 17:57:21', 'File Renamed', 'Renamed file to FORM 8 WITH and COE.docx in folder ID 0'),
+(49, 'Aean Gabrielle D. Tayawa', 'Admin', '2024-05-24 18:05:40', 'Shared a File', 'Shared file ID 20 with new description: testing');
 
 -- --------------------------------------------------------
 
@@ -90,7 +77,11 @@ INSERT INTO `files` (`id`, `name`, `description`, `user_id`, `folder_id`, `file_
 (4, 'New Microsoft Word Document', '', 1, 10, 'docx', '1715174520_New Microsoft Word Document.docx', 0, '2024-05-08 21:22:52'),
 (5, '1b90', 'tite', 1, 10, 'pdf', '1715174580_1b90.pdf', 0, '2024-05-08 21:23:51'),
 (6, '1CanjKtPVzu_8JAWsBVGdpRuEwKJso7U-jVN8TE9JjD0', '', 1, 11, 'pdf', '1715174700_1CanjKtPVzu_8JAWsBVGdpRuEwKJso7U-jVN8TE9JjD0.pdf', 0, '2024-05-08 21:25:02'),
-(7, 'fms_db', 'hehe', 8, 12, 'sql', '1715355000_fms_db.sql', 1, '2024-05-10 23:30:39');
+(17, 'TCW-CHAPTER-1', '', 8, 12, 'pptx', '1715433540_TCW-CHAPTER-1.pptx', 0, '2024-05-11 21:19:54'),
+(18, 'Chapter-I', '', 8, 12, 'docx', '1715433660_Chapter-I.docx', 0, '2024-05-11 21:21:07'),
+(19, 'Chapter-I ||1', '', 8, 12, 'docx', '1715433900_Chapter-I.docx', 0, '2024-05-11 21:25:15'),
+(20, 'FORM 8 WITH and COE', 'testing', 5, 0, 'docx', '1716564060_FORM 8 WITH ENGINEER and COE.docx', 1, '2024-05-25 00:05:40'),
+(25, 'FORM 8 WITH ENGINEER and COE', 'test', 5, 14, 'docx', '1716565260_FORM 8 WITH ENGINEER and COE.docx', 0, '2024-05-24 23:41:57');
 
 -- --------------------------------------------------------
 
@@ -119,7 +110,9 @@ INSERT INTO `folders` (`id`, `user_id`, `name`, `parent_id`) VALUES
 (9, 3, 'My Folder 1', 0),
 (10, 1, 'subfolder', 6),
 (11, 1, 'tite', 8),
-(12, 8, 'test', 0);
+(12, 8, 'test', 0),
+(14, 5, 'Test', 0),
+(15, 5, 'Okayed', 0);
 
 -- --------------------------------------------------------
 
@@ -129,22 +122,30 @@ INSERT INTO `folders` (`id`, `user_id`, `name`, `parent_id`) VALUES
 
 CREATE TABLE `users` (
   `id` int(30) NOT NULL,
+  `profile_pic` varchar(128) NOT NULL,
   `name` varchar(200) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1+admin , 2 = users'
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1+admin , 2 = users',
+  `contact_number` varchar(20) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `address` text NOT NULL,
+  `position` varchar(50) NOT NULL,
+  `birthday` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `civil_status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
-(5, 'Aean Gabrielle D. Tayawa', 'tayawaaean', '202cb962ac59075b964b07152d234b70', 1),
-(8, 'Micheal Jay Pedronan', 'mekel', '202cb962ac59075b964b07152d234b70', 2),
-(11, 'Kenric Catiwa', 'kenken', '202cb962ac59075b964b07152d234b70', 2),
-(12, 'Kimberly Reodique', 'kimmy', '827ccb0eea8a706c4c34a16891f84e7b', 2),
-(13, 'Ryan Anthony Gabriel Adaya', 'ryan', '827ccb0eea8a706c4c34a16891f84e7b', 2);
+INSERT INTO `users` (`id`, `profile_pic`, `name`, `username`, `password`, `type`, `contact_number`, `email`, `address`, `position`, `birthday`, `gender`, `civil_status`) VALUES
+(5, 'profile_pics/profile_picture_206776.png', 'Aean Gabrielle D. Tayawa', 'tayawaaean', '202cb962ac59075b964b07152d234b70', 1, '', '', '', '', '', '', ''),
+(8, 'profile_pics/profile_picture_550633.png', 'Micheal Jay Pedronan', 'mekel', '202cb962ac59075b964b07152d234b70', 2, '095668317230', 'mekel@gmail.com', 'Batac City', 'President', '2024-05-29', 'male', 'single'),
+(11, 'profile_picture_672602.png', 'Kenric Catiwa', 'kenken', '202cb962ac59075b964b07152d234b70', 2, '', '', '', '', '', '', ''),
+(13, '', 'Ryan Anthony Gabriel Adaya', 'ryan', '827ccb0eea8a706c4c34a16891f84e7b', 2, '', '', '', '', '', '', ''),
+(20, 'profile_pics/profile_picture_614861.jpg', 'Jandel Jade Tejada', 'jjadetejada', '202cb962ac59075b964b07152d234b70', 0, '', '', '', '', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -182,25 +183,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `activity_log`
 --
 ALTER TABLE `activity_log`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `folders`
 --
 ALTER TABLE `folders`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
